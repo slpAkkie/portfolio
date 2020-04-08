0a069968ae2a8aa8ec1ad85561169d62d1a3f919
slpAkkie <slpMouse@yandex.ru>
2020-04-08 21:33:09 +0300

    Re-created

---
+++ tajam/src/js/app.js
@@ -0,0 +1,9 @@
+/**
+ * third-party scripts
+ */
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+/**
+ * custom scripts
+ */
+//= partials/_app.js
---
+++ tajam/src/js/partials/_app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+$( document ).ready( function () {
+
+  $( window ).scroll( function () {
+    if ( $( this ).scrollTop() >= 150 ) {
+      $( 'nav.fixed-top' ).addClass( 'scrolled' );
+    } else {
+      $( 'nav.fixed-top' ).removeClass( 'scrolled' );
+    }
+  } );
+
+} );
