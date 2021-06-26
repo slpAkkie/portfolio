b1f30dc2741f2759975bc59efe0f72adb6107ca0
Alexandr Shamanin <sha.alexandr.se@yandex.ru>
2021-06-26 20:43:57 +0300

    Update: paralax effect

--- brella/assets/js/landing.js
+++ brella/assets/js/landing.js
@@ -1,6 +1,19 @@
 let nav = document.querySelector( 'nav.nav' );
+let header = document.querySelector( 'header.header' );
+let forEmployers = document.querySelector( '.for-employers' );
 
 window.addEventListener( 'scroll', function () {
+
+  // Nav background
   if ( window.pageYOffset > 50 ) nav.classList.add( 'nav_scrolled' );
   else nav.classList.remove( 'nav_scrolled' );
+
+  // Header paralax
+  let posHeaderY = window.pageYOffset / 2;
+  header.style.backgroundPositionY = `${posHeaderY}px`;
+
+  // For employers paralax
+  let poForEmployersY = ( window.pageYOffset - forEmployers.offsetTop ) / 5;
+  forEmployers.style.backgroundPositionY = `${poForEmployersY}px`;
+
 } );
